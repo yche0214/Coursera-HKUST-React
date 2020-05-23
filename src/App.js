4268ac0fc2ecb0043faab8d29b59ce27266a8c68
yche0214 <yche0214@student.monash.edu>
2020-05-23 13:46:00 +1000

    intro to redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/ConfigureStore';
+
+
+const store = ConfigureStore();
 
 class App extends Component {
 
@@ -10,12 +15,14 @@ class App extends Component {
 
   render() {
     return (
-      //adding BrowserRouter. the application now is congigured to make use of the react router
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        {/*adding BrowserRouter. the application now is congigured to make use of the react router*/}
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
